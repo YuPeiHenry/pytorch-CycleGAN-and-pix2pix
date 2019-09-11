4eecc80bbf078f97888261f3c7150b321cf8009d
Yu Pei Henry <yupeihenry@gmail.com>
2019-09-11 10:47:51 +0800

    Revert back to paired data sampler.

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -29,8 +29,7 @@ class Pix2PixModel(BaseModel):
         By default, we use vanilla GAN loss, UNet with batchnorm, and aligned datasets.
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
-        #parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
-        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='twocat')
+        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='aligned')
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
@@ -83,8 +82,6 @@ class Pix2PixModel(BaseModel):
         AtoB = self.opt.direction == 'AtoB'
         self.real_A = input['A' if AtoB else 'B'].to(self.device)
         self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.random_A = input['randomA' if AtoB else 'randomB'].to(self.device)
-        self.random_B = input['randomB' if AtoB else 'randomA'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
