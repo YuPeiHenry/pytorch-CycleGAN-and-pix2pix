54a09996a0c13461fa7b76bb24c687c65b3e33e3
Yu Pei Henry <yupeihenry@gmail.com>
2019-09-28 15:04:16 +0800

    Add unet generator without discriminator.

---
+++ models/unet_model.py
@@ -0,0 +1,92 @@
+import torch
+from .base_model import BaseModel
+from . import networks
+import numpy as np
+
+
+class UnetModel(BaseModel):
+    """ This class implements the pix2pix model, for learning a mapping from input images to output images given paired data.
+
+    The model training requires '--dataset_mode aligned' dataset.
+    By default, it uses a '--netG unet256' U-Net generator,
+    a '--netD basic' discriminator (PatchGAN),
+    and a '--gan_mode' vanilla GAN loss (the cross-entropy objective used in the orignal GAN paper).
+
+    pix2pix paper: https://arxiv.org/pdf/1611.07004.pdf
+    """
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        parser.set_defaults(norm='batch', netG='unet_256', dataset_mode='erosion', input_nc=3, output_nc=1, preprocess='N.A.', image_type='uint16', image_value_bound=26350, no_flip=True)
+        parser.add_argument('--fixed_example', action='store_true', help='')
+        parser.add_argument('--fixed_index', type=int, default=0, help='')
+        return parser
+
+    def __init__(self, opt):
+        """Initialize the pix2pix class.
+
+        Parameters:
+            opt (Option class)-- stores all the experiment flags; needs to be a subclass of BaseOptions
+        """
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        self.loss_names = ['G_L2']
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        self.visual_names = ['real_A', 'fake_B', 'real_B']
+        # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
+        self.model_names = ['G']
+        # define networks
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids)
+
+        if self.isTrain:
+            # define loss functions
+            self.criterionL2 = torch.nn.MSELoss()
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap images in domain A and domain B.
+        """
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        self.fake_B = self.netG(self.real_A)  # G(A)
+
+    def backward_D(self):
+        self.loss_D = torch.zeros([1]).to(self.device)
+
+    def backward_G(self):
+        """Calculate GAN and L1 loss for the generator"""
+        # Second, G(A) = B
+        self.loss_G_L2 = self.criterionL2(self.fake_B, self.real_B)
+        # combine loss and calculate gradients
+        self.loss_G = self.loss_G_L2
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        self.forward()                   # compute fake images: G(A)
+        self.backward_D()
+        # update G
+        self.optimizer_G.zero_grad()        # set G's gradients to zero
+        self.backward_G()                   # calculate graidents for G
+        self.optimizer_G.step()             # udpate G's weights
+
+    def compute_visuals(self, dataset=None):
+        if not self.opt.fixed_example or dataset is None:
+            return
+        single = dataset.__getitem__(self.opt.fixed_index)
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = single['A' if AtoB else 'B'].unsqueeze(0).to(self.device)
+        self.real_B = single['B' if AtoB else 'A'].unsqueeze(0).to(self.device)
+        self.image_paths = [single['A_paths' if AtoB else 'B_paths']]
+        self.forward()
