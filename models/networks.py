92b55c97e4ce0da3ec44db4ceb798766f26f4e98
Yu Pei Henry <yupeihenry@gmail.com>
2019-10-03 12:57:33 +0800

    Fix typo.

--- models/networks.py
+++ models/networks.py
@@ -1121,7 +1121,7 @@ class ModifiedUnetBlock(nn.Module):
         self.up = nn.Sequential(*up)
         self.submodule = submodule
         inconv = [nn.Conv2d(input_nc, outer_nc, kernel_size=3, stride=1, padding=1),
-            nn.Conv2d(input_nc, outer_nc, kernel_size=3, stride=1, padding=1), norm_layer(outer_nc)]
+            nn.Conv2d(outer_nc, outer_nc, kernel_size=3, stride=1, padding=1), norm_layer(outer_nc)]
         self.inconv = nn.Sequential(*inconv)
         self.inconv_scalar = torch.cuda.FloatTensor(1)
         self.inconv_scalar.requires_grad = True
