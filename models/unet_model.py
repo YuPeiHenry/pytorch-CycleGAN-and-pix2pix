d4bfed842926d547f0b5ae019b291edea2ab94f2
Yu Pei Henry <yupeihenry@gmail.com>
2019-10-07 10:11:32 +0800

    Fix typo.

--- models/unet_model.py
+++ models/unet_model.py
@@ -63,7 +63,7 @@ class UnetModel(BaseModel):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG(self.real_A)  # G(A)
         if self.opt.generate_residue:
-            self.fake_B = self.fake_B + self.real_A[:, 1, :, :].view(-1, 1, self.fake_B.size[2], self.fake_B.size[3])
+            self.fake_B = self.fake_B + self.real_A[:, 1, :, :].view(-1, 1, self.fake_B.size()[2], self.fake_B.size()[3])
 
     def backward_D(self):
         self.loss_D = torch.zeros([1]).to(self.device)
