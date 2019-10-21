746e79bd9359f9eb9d7fe09abb360e893cfb23dc
Yu Pei Henry <yupeihenry@gmail.com>
2019-10-21 13:45:20 +0800

    Fix saving error for ErosionLayer in UnetModel.

--- models/unet_model.py
+++ models/unet_model.py
@@ -26,7 +26,7 @@ class UnetModel(BaseModel):
         # define networks
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm_G,
                                       not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, downsample_mode=opt.downsample_mode, upsample_mode=opt.upsample_mode, upsample_method=opt.upsample_method)
-        self.netErosion = networks.ErosionLayer(opt.width, opt.iterations)
+        self.netErosion = networks.init_net(networks.ErosionLayer(opt.width, opt.iterations), gpu_ids=[self.device])
 
         if self.isTrain:
             # define loss functions
