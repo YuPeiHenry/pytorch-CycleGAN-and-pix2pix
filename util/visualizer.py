43f16ca34b2cd9c9133c93ab4c6b44aebb64803a
Yu Pei Henry <yupeihenry@gmail.com>
2019-12-10 18:18:11 +0800

    Fix typo.

--- util/visualizer.py
+++ util/visualizer.py
@@ -13,7 +13,7 @@ else:
     VisdomExceptionBase = ConnectionError
 
 
-def save_images(webpage, visuals, image_path, aspect_ratio=1.0, width=256, dataset):
+def save_images(webpage, visuals, image_path, dataset, aspect_ratio=1.0, width=256):
     """Save images to the disk.
 
     Parameters:
