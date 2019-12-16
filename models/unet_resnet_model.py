7fd327d7fddefe573e527c02ae202534990f1350
Yu Pei Henry <yupeihenry@gmail.com>
2019-12-16 11:15:46 +0800

    Add unet_resnet model for terrain erosion.

---
+++ models/unet_resnet_model.py
@@ -0,0 +1,182 @@
+import torch
+from .base_model import BaseModel
+from . import networks
+import numpy as np
+import os
+
+
+class UnetResnetModel(BaseModel):
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        parser.set_defaults(norm='instance', norm_G='instance', netG='resnet_9blocks', dataset_mode='exr', input_nc=5, output_nc=2, preprocess='N.A.', image_type='exr', image_value_bound=26350, no_flip=True)
+        parser.add_argument('--unet_residue', action='store_true', help='')
+        parser.add_argument('--fixed_example', action='store_true', help='')
+        parser.add_argument('--fixed_index', type=int, default=0, help='')
+        parser.add_argument('--netU', type=str, default='unet_256')
+        parser.add_argument('--unet_input_nc', type=int, default=3)
+        parser.add_argument('--width', type=int, default=512)
+        parser.add_argument('--iterations', type=int, default=10)
+        parser.add_argument('--break4', action='store_true', help='')
+        return parser
+
+    def __init__(self, opt):
+        BaseModel.__init__(self, opt)
+        # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
+        self.loss_names = ['D_L2', 'G_L2']
+        # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
+        self.visual_names = ['real_A', 'post_unet', 'fake_B', 'real_B']
+        # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
+        self.model_names = ['G', 'D']
+        self.preload_names = ['U']
+        # define networks
+        self.netU = networks.define_G(opt.unet_input_nc, opt.output_nc, opt.ngf, opt.netU, opt.norm_G,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, downsample_mode=opt.downsample_mode, upsample_mode=opt.upsample_mode, upsample_method=opt.upsample_method, linear=opt.linear)
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm_G,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, downsample_mode=opt.downsample_mode, upsample_mode=opt.upsample_mode, upsample_method=opt.upsample_method, linear=opt.linear)
+        if self.isTrain:  # define a discriminator; conditional GANs need to take both input and output images; Therefore, #channels for D is input_nc + output_nc
+            self.netD = networks.define_D(opt.input_nc + opt.output_nc, opt.ndf, opt.netD,
+                                          opt.n_layers_D, opt.norm, opt.init_type, opt.init_gain, self.gpu_ids, progressive=opt.progressive, progressive_stages=opt.progressive_stages, upsample_method=opt.upsample_method)
+        self.load_base_networks()
+
+        if self.isTrain:
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode, relativistic=opt.relativistic).to(self.device)
+            self.criterionL1 = torch.nn.L1Loss()
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input (dict): include the data itself and its metadata information.
+
+        The option 'direction' can be used to swap images in domain A and domain B.
+        """
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = input['A' if AtoB else 'B'].to(self.device)
+        self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        self.real_B = 1 - torch.nn.ReLU()(2 - torch.nn.ReLU()(self.real_B + 1)) #clip to [-1, 1]
+        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+
+    def forward(self):
+        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+        if self.opt.break4:
+            self.real_A = self.break_into_4(self.real_A)
+            self.real_B = self.break_into_4(self.real_B)
+        self.post_unet = self.netU(self.real_A).detach()
+        if self.opt.unet_residue:
+            self.post_unet[:, 1, :, :] = self.post_unet[:, 1, :, :] + self.real_A[:, 0, :, :]
+        self.fake_B = self.netG(torch.cat((self.real_A, self.post_unet), 1)
+        self.fake_B[:, 1, :, :] = self.fake_B[:, 1, :, :] + self.real_A[:, 0, :, :]
+
+    def backward_D(self):
+        """Calculate GAN loss for the discriminator"""
+        # Fake; stop backprop to the generator by detaching fake_B
+        fake_AB = torch.cat((self.real_A, self.post_unet, self.fake_B), 1)  # we use conditional GANs; we need to feed both input and output to the discriminator
+        pred_fake = self.netD(fake_AB.detach())
+        # Real
+        real_AB = torch.cat((self.real_A, self.post_unet, self.real_B), 1)
+        pred_real = self.netD(real_AB)
+
+        if self.opt.relativistic:
+            loss_D_real, loss_D_fake = self.criterionGAN(pred_real, None, pred_fake, discriminator=True)
+            self.loss_D_fake = loss_D_fake
+            self.loss_D_real = loss_D_real
+        else:
+            self.loss_D_fake = self.criterionGAN(pred_fake, False)
+            self.loss_D_real = self.criterionGAN(pred_real, True)
+        # combine loss and calculate gradients
+        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+        self.loss_D.backward()
+
+    def backward_G(self):
+        """Calculate GAN and L1 loss for the generator"""
+        # First, G(A) should fake the discriminator
+        fake_AB = torch.cat((self.real_A, self.post_unet, self.fake_B), 1)
+        pred_fake = self.netD(fake_AB)
+
+        if self.opt.relativistic:
+            # Real
+            real_AB = torch.cat((self.real_A, self.post_unet, self.real_B), 1)
+            pred_real = self.netD(real_AB).detach()
+            self.loss_G_GAN = self.criterionGAN(pred_real, None, pred_fake, discriminator=False)
+        else:
+            self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+        # Second, G(A) = B
+        self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
+        # combine loss and calculate gradients
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        self.loss_G.backward()
+
+    def optimize_parameters(self):
+        self.forward()                   # compute fake images: G(A)
+        # update D
+        self.set_requires_grad(self.netD, True)  # enable backprop for D
+        self.optimizer_D.zero_grad()     # set D's gradients to zero
+        self.backward_D()                # calculate gradients for D
+        self.optimizer_D.step()          # update D's weights
+        # update G
+        self.set_requires_grad(self.netD, False)  # D requires no gradients when optimizing G
+        self.optimizer_G.zero_grad()        # set G's gradients to zero
+        self.backward_G()                   # calculate graidents for G
+        self.optimizer_G.step()             # udpate G's weights
+
+    def compute_visuals(self, dataset=None):
+        if not self.opt.fixed_example or dataset is None:
+            return
+        single = dataset.dataset.get_val_item(self.opt.fixed_index)
+        AtoB = self.opt.direction == 'AtoB'
+        self.real_A = single['A' if AtoB else 'B'].unsqueeze(0).to(self.device)
+        self.real_B = single['B' if AtoB else 'A'].unsqueeze(0).to(self.device)
+        self.image_paths = [single['A_paths' if AtoB else 'B_paths']]
+
+        self.forward()
+        if self.opt.break4:
+            self.real_A = self.combine_from_4(self.real_A)
+            self.real_B = self.combine_from_4(self.real_B)
+            self.post_unet = self.combine_from_4(self.post_unet)
+            self.fake_B = self.combine_from_4(self.fake_B)
+
+    def __patch_instance_norm_state_dict(self, state_dict, module, keys, i=0):
+        """Fix InstanceNorm checkpoints incompatibility (prior to 0.4)"""
+        key = keys[i]
+        if i + 1 == len(keys):  # at the end, pointing to a parameter/buffer
+            if module.__class__.__name__.startswith('InstanceNorm') and \
+                    (key == 'running_mean' or key == 'running_var'):
+                if getattr(module, key) is None:
+                    state_dict.pop('.'.join(keys))
+            if module.__class__.__name__.startswith('InstanceNorm') and \
+               (key == 'num_batches_tracked'):
+                state_dict.pop('.'.join(keys))
+        else:
+            self.__patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
+
+    def load_base_networks(self):
+        for name in self.preload_names:
+            if isinstance(name, str):
+                load_filename = 'base_net_%s.pth' % (name)
+                load_path = os.path.join(self.save_dir, load_filename)
+                net = getattr(self, 'net' + name)
+                if isinstance(net, torch.nn.DataParallel):
+                    net = net.module
+                print('loading the model from %s' % load_path)
+                # if you are using PyTorch newer than 0.4 (e.g., built from
+                # GitHub source), you can remove str() on self.device
+                state_dict = torch.load(load_path, map_location=str(self.device))
+                if hasattr(state_dict, '_metadata'):
+                    del state_dict._metadata
+
+                # patch InstanceNorm checkpoints prior to 0.4
+                for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
+                    self.__patch_instance_norm_state_dict(state_dict, net, key.split('.'))
+                net.load_state_dict(state_dict)
+
+    def break_into_4(self, image):
+        return torch.cat(torch.chunk(torch.cat(torch.chunk(image, 2, dim=2), 0), 2, dim=3), 0)
+
+    def combine_from_4(self, image):
+        return torch.cat(torch.chunk(torch.cat(torch.chunk(image, 2, dim=0), 3), 2, dim=0), 2)
