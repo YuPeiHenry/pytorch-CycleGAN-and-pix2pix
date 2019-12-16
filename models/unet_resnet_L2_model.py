b1e5d89a24a0801082ebd515c4d8221b5833aa9b
Yu Pei Henry <yupeihenry@gmail.com>
2019-12-16 12:46:34 +0800

    Fix loss names.

--- models/unet_resnet_L2_model.py
+++ models/unet_resnet_L2_model.py
@@ -20,7 +20,7 @@ class UnetResnetL2Model(BaseModel):
     def __init__(self, opt):
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
+        self.loss_names = ['D_L2', 'G_L2']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         self.visual_names = ['real_A', 'post_unet', 'fake_B', 'real_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
