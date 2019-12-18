d0376356271c15b5c82249d2b2bc50e570f12212
Yu Pei Henry <yupeihenry@gmail.com>
2019-12-18 14:09:44 +0800

    Add missing comma.

--- data/exr_dataset.py
+++ data/exr_dataset.py
@@ -48,7 +48,7 @@ class ExrDataset(BaseDataset):
             #self.o_channels_max = np.array([[[2.76426649, 2.35645199, 418.50262451, 4.86357594, 2.89976954]]])
             self.i_channels_min = np.array([[[0, 0, -0.05163945]]])
             self.i_channels_max = np.array([[[417.75454712, 0.99980736, 1.]]])
-            self.o_channels_min = np.array([[[5.08151024e-05 -3.95739818]]])
+            self.o_channels_min = np.array([[[5.08151024e-05, -3.95739818]]])
             self.o_channels_max = np.array([[[1., 418.50262451]]])
             return
 
