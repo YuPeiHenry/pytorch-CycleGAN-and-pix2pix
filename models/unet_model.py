1a86b98466269e1ce2fdb545117847cc0257ef37
Yu Pei Henry <yupeihenry@gmail.com>
2020-01-14 10:17:06 +0800

    Fix typo.

--- models/unet_model.py
+++ models/unet_model.py
@@ -57,7 +57,7 @@ class UnetModel(BaseModel):
         self.preload_names = []
         # define networks
         self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.norm_G,
-                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, downsample_mode=opt.downsample_mode, upsample_mode=opt.upsample_mode, upsample_method=opt.upsample_method, linear=opt.linear, numDownsampleConv=opt.downsampleConv, numUpsampleConv=opt.upsampleConv, opt.maxFilters)
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, self.gpu_ids, downsample_mode=opt.downsample_mode, upsample_mode=opt.upsample_mode, upsample_method=opt.upsample_method, linear=opt.linear, numDownsampleConv=opt.downsampleConv, numUpsampleConv=opt.upsampleConv, max_filters=opt.maxFilters)
         if opt.use_erosion:
             self.netErosion = networks.init_net(networks.ErosionLayer(opt.width, opt.iterations, opt.erosion_flowmap, opt.erosion_random, opt.blend_inputs, opt.use_rain_conv), gpu_ids=self.gpu_ids)
         if opt.preload_unet:
