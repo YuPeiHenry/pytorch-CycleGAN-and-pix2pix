ee7873485559735bec6d4477b1b8fd17d198e403
Yu Pei Henry <yupeihenry@gmail.com>
2020-01-15 17:25:23 +0800

    Rename model.

--- models/overfit_model.py
+++ models/overfit_model.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import data.exrlib as exrlib
 
-class UnetModel(BaseModel):
+class OverfitModel(BaseModel):
     @staticmethod
     def modify_commandline_options(parser, is_train=True):
         parser.set_defaults(norm='instance', norm_G='instance', netG='overfit', dataset_mode='exr_one_channel', input_nc=1, output_nc=1, preprocess='N.A.', image_type='exr', no_flip=True)
