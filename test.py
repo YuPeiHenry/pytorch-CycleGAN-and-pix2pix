a12359175d2266b7d24f836ea2a37255f6aa8132
Yu Pei Henry <yupeihenry@gmail.com>
2020-01-23 14:22:49 +0800

    Support multithread and batch size for testing.

--- test.py
+++ test.py
@@ -37,8 +37,6 @@ from util import html
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
-    opt.num_threads = 0   # test code only supports num_threads = 1
-    opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
--- util/visualizer.py
+++ util/visualizer.py
@@ -26,6 +26,39 @@ def save_images(webpage, visuals, image_path, dataset, aspect_ratio=1.0, width=2
     This function will save images stored in 'visuals' to the HTML file specified by 'webpage'.
     """
     image_dir = webpage.get_image_dir()
+    if not isinstance(image_path, list):
+        return save_images_single(webpage, visuals, image_path, dataset, aspect_ratio, width)
+    short_paths = [ntpath.basename(single_path) for single_path in image_path]
+    names = [os.path.splitext(path)[0] for short_path in short_paths]
+
+    for i in len(names):
+        name = names[i]
+        webpage.add_header(name)
+        ims, txts, links = [], [], []
+
+        for label, im_data in visuals.items():
+            im = util.tensor2im(im_data[i].unsqueeze(0))
+            image_name = ('%s_%s.png' % (name, label[i])) if label[i] != "" else ('%s.png' % (name))
+            save_path = os.path.join(image_dir, image_name)
+            util.save_image(im, save_path, dataset, aspect_ratio=aspect_ratio)
+            ims.append(image_name)
+            txts.append(label[i])
+            links.append(image_name)
+        webpage.add_images(ims, txts, links, width=width)
+
+def save_images_single(webpage, visuals, image_path, dataset, aspect_ratio=1.0, width=256):
+    """Save images to the disk.
+
+    Parameters:
+        webpage (the HTML class) -- the HTML webpage class that stores these imaegs (see html.py for more details)
+        visuals (OrderedDict)    -- an ordered dictionary that stores (name, images (either tensor or numpy) ) pairs
+        image_path (str)         -- the string is used to create image paths
+        aspect_ratio (float)     -- the aspect ratio of saved images
+        width (int)              -- the images will be resized to width x width
+
+    This function will save images stored in 'visuals' to the HTML file specified by 'webpage'.
+    """
+    image_dir = webpage.get_image_dir()
     short_path = ntpath.basename(image_path[0])
     name = os.path.splitext(short_path)[0]
 
