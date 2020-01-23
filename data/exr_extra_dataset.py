ab8352734ade82e4463bf3a8e2dd4d4b70d46098
Yu Pei Henry <yupeihenry@gmail.com>
2020-01-23 14:08:44 +0800

    Fix typo.

--- data/exr_extra_dataset.py
+++ data/exr_extra_dataset.py
@@ -39,7 +39,7 @@ class ExrExtraDataset(ExrDataset):
         dict = super().__getitem__(index)
         Extra_path = self.Extra_paths[index % self.Extra_size]
         Extra_img = exrlib.read_exr_float32(Extra_path, list(['0', '1']), 512, 512)
-        dict["Extra"] = Extra
+        dict["Extra"] = Extra_img
 
         return dict
 
@@ -47,6 +47,6 @@ class ExrExtraDataset(ExrDataset):
         dict = super().get_val_item(index)
         Extra_path = self.Extra_test_paths[index % self.Extra_size]
         Extra_img = exrlib.read_exr_float32(Extra_path, list(['0', '1']), 512, 512)
-        dict["Extra"] = Extra
+        dict["Extra"] = Extra_img
 
         return dict
