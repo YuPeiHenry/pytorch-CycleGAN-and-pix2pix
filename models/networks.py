ae494e85d41a10f144a696a5eb0224c4e211961a
Yu Pei Henry <yupeihenry@gmail.com>
2020-03-04 18:19:22 +0800

    Fix typo.

--- models/networks.py
+++ models/networks.py
@@ -1203,9 +1203,9 @@ class UnetResBlockSplit(nn.Module):
         intermediate = x
         intermediates = []
         for i in range(self.depth):
-            intermediate = getattr(self, "b1_conv1_" + str(i))(x)
+            intermediate = getattr(self, "b1_conv1_" + str(i))(intermediate)
             intermediates += [intermediate]
-            intermediate = getattr(self, "b1_down_" + str(i))(x)
+            intermediate = getattr(self, "b1_down_" + str(i))(intermediate)
         
         upsample_intermediate = self.bottleneck(torch.cat((intermediate, slope_embedding), 1))
         for i in range(self.depth):
